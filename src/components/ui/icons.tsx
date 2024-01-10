44b4f6c96448d54b9b420c95569ea41f06357f0b
Renan Oliveira <rotrenanoliveira@gmail.com>
2024-01-10 12:02:16 -0300

    feat: adding new icons

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -10,6 +10,14 @@ const Icon = React.forwardRef<SVGSVGElement, IconProps>(({ size = 32, ...props }
 
 Icon.displayName = 'Icon'
 
+function Moon({ ...props }: IconProps) {
+  return (
+    <Icon xmlns="http://www.w3.org/2000/svg" {...props} viewBox="0 0 256 256">
+      <path d="M230.72,145.06a4,4,0,0,0-4-1A92.08,92.08,0,0,1,111.94,29.27a4,4,0,0,0-5-5A100.78,100.78,0,0,0,56.08,59.88a100,100,0,0,0,140,140,100.78,100.78,0,0,0,35.59-50.87A4,4,0,0,0,230.72,145.06ZM191.3,193.53A92,92,0,0,1,62.47,64.7a93,93,0,0,1,39.88-30.35,100.09,100.09,0,0,0,119.3,119.3A93,93,0,0,1,191.3,193.53Z"></path>
+    </Icon>
+  )
+}
+
 function Play({ ...props }: IconProps) {
   return (
     <Icon xmlns="http://www.w3.org/2000/svg" {...props} viewBox="0 0 256 256">
@@ -42,4 +50,12 @@ function Stop({ ...props }: IconProps) {
   )
 }
 
-export { Play, Pause, ResetTimer, Stop }
+function Sun({ ...props }: IconProps) {
+  return (
+    <Icon xmlns="http://www.w3.org/2000/svg" {...props} viewBox="0 0 256 256">
+      <path d="M124,40V16a4,4,0,0,1,8,0V40a4,4,0,0,1-8,0Zm64,88a60,60,0,1,1-60-60A60.07,60.07,0,0,1,188,128Zm-8,0a52,52,0,1,0-52,52A52.06,52.06,0,0,0,180,128ZM61.17,66.83a4,4,0,0,0,5.66-5.66l-16-16a4,4,0,0,0-5.66,5.66Zm0,122.34-16,16a4,4,0,0,0,5.66,5.66l16-16a4,4,0,0,0-5.66-5.66ZM192,68a4,4,0,0,0,2.83-1.17l16-16a4,4,0,1,0-5.66-5.66l-16,16A4,4,0,0,0,192,68Zm2.83,121.17a4,4,0,0,0-5.66,5.66l16,16a4,4,0,0,0,5.66-5.66ZM40,124H16a4,4,0,0,0,0,8H40a4,4,0,0,0,0-8Zm88,88a4,4,0,0,0-4,4v24a4,4,0,0,0,8,0V216A4,4,0,0,0,128,212Zm112-88H216a4,4,0,0,0,0,8h24a4,4,0,0,0,0-8Z"></path>
+    </Icon>
+  )
+}
+
+export { Moon, Play, Pause, ResetTimer, Stop, Sun }
